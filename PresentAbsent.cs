080cde64ca710174f5d4b76333fe0367dbf86e54
yashdondal <yashdondal21@gmail.com>
2022-10-06 21:51:39 +0530

    Present or Absent

---
+++ PresentAbsent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    public static class PresentAbsent
+    {
+        public static void EmployeeCheckAttendance()
+        {
+            int Is_FullTime = 1;
+            Random random = new Random();
+            int emplCheck = random.Next(0, 2);
+
+            if (emplCheck == 1)
+            {
+                Console.WriteLine("Empolyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
+    
