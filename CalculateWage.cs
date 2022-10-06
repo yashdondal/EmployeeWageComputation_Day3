7e03e331ac91dab1f6ad13a91dc9895926e177d1
yashdondal <yashdondal21@gmail.com>
2022-10-06 22:02:21 +0530

    calculate wage

---
+++ CalculateWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    public static class CalculateWage
+    {
+        public static void ToCalculate_EmpWage()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_PerHr = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+
+
+            Random random = new Random();
+            int emplCheck = random.Next(2);
+
+            if (emplCheck == Is_Full_Time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWages = empHrs * Emp_Rate_PerHr;
+            Console.WriteLine("Employee Daily Wages are : " + empWages);
+        }
+    }
+}
+ 
