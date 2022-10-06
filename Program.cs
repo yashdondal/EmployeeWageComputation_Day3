2b425cc03680f357d17fa21ee64046475ddea5b7
yashdondal <yashdondal21@gmail.com>
2022-10-06 21:33:25 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeWages;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Employee Wage Computataion Problem");
+            //PresentAbsent.EmployeeCheckAttendance();
+            //CalculateWage.ToCalculate_EmpWage();
+            //PartTimeWage.ToCalculate_EmpWage();
+            //SwitchCase.UsingSwitch();
+            //MonthEmpWage.ToCalculate_EmpWage(); 
+            TotalWages.Max_Month_Wages();
+        }
+    }
+}
