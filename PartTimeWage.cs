c7567dbeea561d89aff17042344406b9e8c7179d
yashdondal <yashdondal21@gmail.com>
2022-10-06 21:54:47 +0530

    Part Time Wage

---
+++ PartTimeWage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class PartTimeWage
+    {
+        public static void ToCalculate_EmpWage()
+        {
+            int Is_FullTime = 1;
+            int Is_PartTime = 2;
+            int EmpWage_PerHr = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == Is_FullTime)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == Is_PartTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EmpWage_PerHr;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
